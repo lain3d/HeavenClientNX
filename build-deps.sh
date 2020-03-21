225779412b3bfb6ca8afd0d8289e232a424a883f
lain3d <eyeofsteiner@gmail.com>
2020-03-21 04:40:09 -0500

    add script to build deps

---
+++ build-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd libs
+source /opt/devkitpro/switchvars.sh
+cd NoLifeNx/lz4-1.8.2
+make -f Makefile.nx && \
+cd ../ && ./build-switch.sh && \
+cd ../glad && make && \
+cd ../glfw && ./build.sh && \
+cd ../openal-soft && make -f Makefile.nx && \
+cd ../alure && ./build-switch.sh 
+
