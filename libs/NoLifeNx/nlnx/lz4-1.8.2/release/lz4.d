a681f17d62b4c57287f4d52a2b9aefee51aeb2ce
lain3d <eyeofsteiner@gmail.com>
2020-04-12 18:19:44 -0500

    remove subrepo

--- libs/NoLifeNx/nlnx/lz4-1.8.2/release/lz4.d
+++
@@ -1,4 +0,0 @@
-lz4.o: /home/rich/HeavenClientNX/libs/NoLifeNx/lz4-1.8.2/lib/lz4.c \
- /home/rich/HeavenClientNX/libs/NoLifeNx/lz4-1.8.2/lib/lz4.h
-
-/home/rich/HeavenClientNX/libs/NoLifeNx/lz4-1.8.2/lib/lz4.h:
--- libs/NoLifeNx/nlnx/lz4-1.8.2/release/xxhash.d
+++
@@ -1,4 +0,0 @@
-xxhash.o: /home/rich/HeavenClientNX/libs/NoLifeNx/lz4-1.8.2/lib/xxhash.c \
- /home/rich/HeavenClientNX/libs/NoLifeNx/lz4-1.8.2/lib/xxhash.h
-
-/home/rich/HeavenClientNX/libs/NoLifeNx/lz4-1.8.2/lib/xxhash.h:
